40c2fa44a60bf9bb7a398011307d9fc9222b954c
Jake Sheets <jake@thesheets.me>
2023-03-30 23:18:46 -0400

    Disable netlify SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-import preact from "@astrojs/preact";
+// // https://astro.build/config
+// import preact from "@astrojs/preact";
 
-// https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+// // https://astro.build/config
+// import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
-  output: "server",
-  adapter: netlify()
+  integrations: [tailwind()],
+  // adapter: netlify(),
 });
